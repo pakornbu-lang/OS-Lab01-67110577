8f33de59d3e6fe971f8a6eee59d114bdbe4d358b
Pakorn Bunkongmak <pakorn.bu@mail.wu.ac.th>
2026-08-30 17:32:12 +0000

    Completed Lab 1: System Profiling

--- stress_test.py
+++ stress_test.py
@@ -1,8 +1,12 @@
 # ==========================================
 # OS-Lab 01: CPU Stress Test
-# Student ID: [Enter your ID here]
+# Student ID: [ 67110577 ] 
 # ==========================================
-import time
+import math
 
 # TODO: Write the intensive computation loop here
 # Follow the instructions in the Lab manual.
+
+print("Starting CPU Stress Test... Press Ctrl+C to stop.")
+while True:
+    math.factorial(50000)
